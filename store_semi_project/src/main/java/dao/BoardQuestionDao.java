37e4d0550e3b3410dcc48de2ec63b02acb74cb68
SongYeJi <zhah0423@gmail.com>
2023-06-07 12:10:05 +0900

    [송예지] BoardQuestionDao selectBoardQuestionByPage Method 추가

--- store_semi_project/src/main/java/dao/BoardQuestionDao.java
+++ store_semi_project/src/main/java/dao/BoardQuestionDao.java
@@ -2,11 +2,124 @@ package dao;
 
 import util.DBUtil;
 import java.sql.*;
+import java.util.ArrayList;
+
 import vo.*;
 
 public class BoardQuestionDao {
 	
-	// 조회
+	// RESET ANST CODE 콘솔창 글자색, 배경색 지정
+		final String RESET = "\u001B[0m";
+		final String BLUE ="\u001B[34m";
+		final String BG_YELLOW ="\u001B[43m";
+		
+	/* 조회
+	 * 1. 전체 조회 (searchWord.equals(""))
+	 * 2. 제목+내용, searchWord (searchWord.equals("") == false && columnName.equals("titleContent"))
+	 * 3. 제목, searchWord (searchWord.equals("") == false && columnName.equals("title"))
+	 * 4. 내용, searchWord (searchWord.equals("") == false && columnName.equals("content"))
+	 * 5. 작성자, searchWord (searchWord.equals("") == false && columnName.equals("id"))
+	 
+	 * String searchWord : sql = Like ?(searchWord)
+	 * String columnName : columnName.equals()
+	*/
+	
+	public ArrayList<BoardQuestion> selectBoardQuestionByPage
+	(int beginRow, int rowPerPage, String searchWord, String columnName) throws Exception{
+		
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		
+		String sql = null;
+		PreparedStatement stmt = null;
+		
+		if(searchWord.equals("")) { // 1. 전체 조회
+			
+			sql = "SELECT board_q_no boardQNo, id, board_q_category boardQCategory, board_q_title boardQTitle, "
+				+ "board_q_content boardQContent, board_q_check_cnt boardQCheckCnt, createdate, updatedate "
+				+ "FROM board_question "
+				+ "ORDER BY createdate DESC";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1,beginRow);
+			stmt.setInt(2,rowPerPage);
+			System.out.println(BG_YELLOW+BLUE+stmt +"<--BoardQuestionDao 1.stmt"+RESET);
+		} else if(searchWord.equals("") == false && columnName.equals("titleContent")){ // 2. 제목+내용, searchWord
+			
+			sql = "SELECT board_q_no boardQNo, id, board_q_category boardQCategory, board_q_title boardQTitle, "
+				+ "board_q_content boardQContent, board_q_check_cnt boardQCheckCnt, createdate, updatedate "
+				+ "FROM board_question "
+				+ "WHERE board_q_title LIKE ? or board_q_content LIKE ? "
+				+ "ORDER BY createdate DESC LIMIT ?, ?";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%"+searchWord+"%");
+			stmt.setString(2, "%"+searchWord+"%");
+			stmt.setInt(3, beginRow);
+			stmt.setInt(4, rowPerPage);
+			System.out.println(BG_YELLOW+BLUE+stmt +"<--BoardQuestionDao 2.stmt"+RESET);
+			
+		} else if(searchWord.equals("") == false && columnName.equals("title")) { // 3. 제목, searchWord
+			
+			sql = "SELECT board_q_no boardQNo, id, board_q_category boardQCategory, board_q_title boardQTitle, "
+				+ "board_q_content boardQContent, board_q_check_cnt boardQCheckCnt, createdate, updatedate "
+				+ "FROM board_question "
+				+ "WHERE board_q_title LIKE ?"
+				+ "ORDER BY createdate DESC LIMIT ?, ?";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%"+searchWord+"%");
+			stmt.setInt(2, beginRow);
+			stmt.setInt(3, rowPerPage);
+			System.out.println(BG_YELLOW+BLUE+stmt +"<--BoardQuestionDao 3.stmt"+RESET);
+			
+		} else if(searchWord.equals("") == false && columnName.equals("content")) { // 4. 내용, searchWord
+			
+			sql = "SELECT board_q_no boardQNo, id, board_q_category boardQCategory, board_q_title boardQTitle, "
+				+ "board_q_content boardQContent, board_q_check_cnt boardQCheckCnt, createdate, updatedate "
+				+ "FROM board_question "
+				+ "WHERE board_q_content LIKE ? "
+				+ "ORDER BY createdate DESC LIMIT ?, ?";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%"+searchWord+"%");
+			stmt.setInt(2, beginRow);
+			stmt.setInt(3, rowPerPage);
+			System.out.println(BG_YELLOW+BLUE+stmt +"<--BoardQuestionDao 4.stmt"+RESET);
+			
+		} else if(searchWord.equals("") == false && columnName.equals("id")) { // 5. 작성자, searchWord
+			
+			sql = "SELECT board_q_no boardQNo, id, board_q_category boardQCategory, board_q_title boardQTitle, "
+				+ "board_q_content boardQContent, board_q_check_cnt boardQCheckCnt, createdate, updatedate "
+				+ "FROM board_question "
+				+ "WHERE id LIKE ? "
+				+ "ORDER BY createdate DESC LIMIT ?, ?";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%"+searchWord+"%");
+			stmt.setInt(2, beginRow);
+			stmt.setInt(3, rowPerPage);
+			System.out.println(BG_YELLOW+BLUE+stmt +"<--BoardQuestionDao 5.stmt"+RESET);	
+		}
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		ArrayList<BoardQuestion> list = new ArrayList<>();
+		while(rs.next()) {
+			BoardQuestion bq = new BoardQuestion();
+			bq.setBoardQNo(rs.getInt("boardQNo"));
+			bq.setId(rs.getString("id"));
+			bq.setBoardQCategory(rs.getString("boardQCategory"));
+			bq.setBoardQTitle(rs.getString("boardQTitle"));
+			bq.setBoardQContent(rs.getString("boardQContent"));
+			bq.setBoardQCheckCnt(rs.getInt("boardQCheckCnt"));
+			bq.setCreatedate(rs.getString("createdate"));
+			bq.setUpdatedate(rs.getString("updatedate"));
+			list.add(bq);
+		}
+		
+		return list;
+	}
 	
 	// 상세 조회(board_question)
 	public BoardQuestion selectBoardQuestionOne(int boardQuestionNo) throws Exception {
